9d3cdf45434366dc1d99a57bcc1512d905327a04
jhallard <jhallard@ucsc.edu>
2015-01-23 18:21:37 -0800

    CS101 PA #2 : About a third of the way through finishing the Lex program, the List is fully tested and debugged except for one dang memory leak

--- CS101/PA/PA_2/Lex.c
+++ CS101/PA/PA_2/Lex.c
@@ -11,13 +11,56 @@
 #include<stdlib.h>
 #include "List.h"
 
+int readFile(char * fn, char **);
 
-int main(int argc. char *argv[]) {
+int getNumLines(char * fn);
 
-    /* argc should be 2 for correct execution */  
+int main(int argc, char *argv[]) {
+
+    /* argc should be 3 for correct execution */  
     if(argc != 3)  {
         fprintf(stderr, "Error, Proper Usage: Lex <in_fn> <out_fn>");
         exit(1);
     }
-    
+
+    char ** word_list = NULL;
+    int num_words = readFile(argv[1], word_list);
+    num_words++;
+
+    return 0;
+
+}
+
+int readFile(char * fn, char ** words) {
+     int current_line = 0;
+     int BUFSIZE = 1000;
+     int num_lines = getNumLines(fn);
+     words = malloc(num_lines*sizeof(char*));
+
+     FILE *fp = fopen("input.txt", "r");
+     if (fp == 0){
+            fprintf(stderr, "Error while opening");
+            exit(1);
+     }
+
+     words[current_line] = malloc(BUFSIZE);
+      while (fgets(words[current_line], BUFSIZE, fp)) {
+            current_line++;
+            words[current_line] = malloc(BUFSIZE);
+     } 
+
+     return num_lines;
+}
+
+int getNumLines(char * fn) {
+    int lines=0;
+    char ch;
+    FILE * fp=fopen(fn,"r");
+    while((ch=fgetc(fp))!=EOF)
+    {
+        if (ch=='\n') { lines++; }
+    }
+    fclose(fp);
+
+    return lines;
 }
